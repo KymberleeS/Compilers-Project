1fb6d48876f822ee3436a4f17ee8a458fb2ec2e6
Harrison Wine <n01313288@osprey.unfcsd.unf.edu>
2022-03-04 18:20:21 -0500

    table implementation
    implemented the table and stack so that values can be added to the current hash table on the stack.

--- Step3/Listener.java
+++ Step3/Listener.java
@@ -14,8 +14,8 @@ public class Listener extends LittleBaseListener {
         }
     }
 
-    Hashtable<Integer, Node> globalHT = new Hashtable<>();
-    Stack<Hashtable> stackHT = new Stack<>();
+    Hashtable<String, Node> globalHT = new Hashtable<>();
+    Stack<Hashtable<String, Node>> stackHT = new Stack<>();
 
     public void enterString_decl(LittleParser.String_declContext ctx) {
         String[] list = ctx.getText().split("[a-z]+");
@@ -32,19 +32,26 @@ public class Listener extends LittleBaseListener {
 
     public void enterPgm_body(LittleParser.Pgm_bodyContext ctx) {
         System.out.println("Symbol table GLOBAL");
+        stackHT.push(globalHT);
     }
     
-
-
     public void enterVar_decl(LittleParser.Var_declContext ctx) {
         String type    = ctx.var_type().getText();
         String name    = "placeholder";
         String id_list = ctx.id_list().getText();
         int    size    = id_list.length();
         String list[]  = id_list.split(",");
+        Hashtable<String, Node> current_table = stackHT.peek();
 
         for(String str : list){
-            System.out.println("name "+str+" type "+type);
+            if(!current_table.containsKey(str)){
+                Node var_decl = new Node(str, type, null);
+                current_table.put(str, var_decl);
+                System.out.println("name "+str+" type "+type);
+            }
+            else{
+               System.out.println("DECLARATION ERROR "+str);
+            }
         }
     }
 }
