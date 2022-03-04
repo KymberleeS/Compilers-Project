ab326295d52f642471b1c1976a057241d220dabf
Harrison Wine <n01313288@osprey.unfcsd.unf.edu>
2022-03-04 15:58:58 -0500

    Outputting ids
    modified enterVar_decl and prints each var in the desired format, currently only works for single char variables.

--- Step3/Listener.java
+++ Step3/Listener.java
@@ -11,11 +11,32 @@ public class Listener extends LittleBaseListener {
     }
 
     public void enterVar_decl(LittleParser.Var_declContext ctx) {
-        System.out.println(ctx.var_type().getText());
+        String type    = ctx.var_type().getText();
+        String name    = "placeholder";
+        String id_list = ctx.id_list().getText();
+        int size       = id_list.length();
+        char list[]    = id_list.toCharArray();
+
+        for(int i = 0; i < size; i++){
+            if(list[i] == ','){
+                continue;
+            }
+            else{
+                System.out.println("name "+list[i]+" type "+type);
+            }
+        }
+
+        /*System.out.println(list[0]);
+        System.out.println("name "+name+" type "+type);
+        System.out.println(ctx.id_list());
+        System.out.println(ctx.id_list().getText());
         System.out.println(ctx.id_list().id().getText());
         System.out.println(ctx.id_list().id_tail().id().getText());
         System.out.println(ctx.id_list().id_tail().id_tail().id().getText());
-        System.out.println(ctx.id_list().id_tail().id_tail().id_tail().id().getText());
+        System.out.println(ctx.id_list().id_tail().id_tail().id_tail().id().getText());*/
     }
 
+    public void enterId_list(LittleParser.Id_listContext ctx) {
+        System.out.println(ctx.getText());
+    }
 }
