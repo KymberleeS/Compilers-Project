ce79cffc65f006951edfc4726dc6d432dfddc361
KymberleeS <sableskymberlee@yahoo.com>
2022-03-04 15:24:30 -0500

    experimenting with isolating variables with their name and type

--- Step3/Listener.java
+++ Step3/Listener.java
@@ -1,7 +1,21 @@
 public class Listener extends LittleBaseListener {
-    public void enterProgram(LittleParser.ProgramContext ctx){
+    /*public void enterProgram(LittleParser.ProgramContext ctx){
         String id = ctx.id().getText();
         String body = ctx.pgm_body().getText();
-        System.out.println(id+"\n"+body);
+        System.out.println(body+"\n");
+    }*/
+
+    public void enterString_decl(LittleParser.String_declContext ctx) {
+        System.out.println(ctx.id().getText());
+        System.out.println(ctx.str().getText());
     }
+
+    public void enterVar_decl(LittleParser.Var_declContext ctx) {
+        System.out.println(ctx.var_type().getText());
+        System.out.println(ctx.id_list().id().getText());
+        System.out.println(ctx.id_list().id_tail().id().getText());
+        System.out.println(ctx.id_list().id_tail().id_tail().id().getText());
+        System.out.println(ctx.id_list().id_tail().id_tail().id_tail().id().getText());
+    }
+
 }
