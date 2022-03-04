f067b15d19f68366f771b30262158e8b3c768135
harrisonwine <harrisonmwine@protonmail.com>
2022-03-04 12:12:27 -0500

    Output enterProgram
    implemented the enterProgram function in Listener.java so that it outputs the name and body of the program

--- Step3/Listener.java
+++ Step3/Listener.java
@@ -1,3 +1,7 @@
 public class Listener extends LittleBaseListener {
-
+    public void enterProgram(LittleParser.ProgramContext ctx){
+        String id = ctx.id().getText();
+        String body = ctx.pgm_body().getText();
+        System.out.println(id+"\n"+body);
+    }
 }
